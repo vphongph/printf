ef8ce2a05cd0a33f353ae4653163436107e947ae
vphongph <vphongph@student.42.fr>
2019-03-07 02:24:28 +0100

    alsmost finish int POW

--- ft_pow_int.c
+++ ft_pow_int.c
@@ -6,7 +6,7 @@
 /*   By: vphongph <vphongph@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/03/05 15:47:58 by vphongph          #+#    #+#             */
-/*   Updated: 2019/03/06 20:22:43 by vphongph         ###   ########.fr       */
+/*   Updated: 2019/03/07 02:21:25 by vphongph         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -54,66 +54,32 @@
 **	}
 */
 
-int64_t	pow_pos_number(int64_t x, int64_t y)
-{
-	int64_t original_x;
-
-	original_x = x;
-	while (--y > 0)
-	{
-		x *= original_x;
-		printf("LOOP llmax / x : %lld\n", LLONG_MAX / x);
-		if (y > 1 && (LLONG_MAX / x) < original_x)
-		{
-			write(1, "POS Result too big\n", 19);
-			return (0);
-		}
-		printf("y IN  : %lld\n", y);
-	}
-	printf("y OUT : %lld\n", y);
-	return (x);
-}
-
-int64_t	pow_neg_number(int64_t x, int64_t y)
-{
-	int64_t original_x;
-	int64_t last_pos_x;
-	int64_t last_neg_x;
-	int64_t i;
-
-	original_x = x;
-	last_neg_x = x;
-	last_pos_x = 0;
-	i = 0;
-	while (++i < y)
-	{
-		x *= original_x;
-		if ((!(i & 1) && last_neg_x < x) || (i & 1 && last_pos_x > x) || x == 0)
-		{
-			write(1, "NEG Result too big\n", 19);
-			return (0);
-		}
-		if (i & 1)
-			last_pos_x = x;
-		else
-			last_neg_x = x;
-	}
-	return (x);
-}
-
 int8_t	check_result(int64_t original_x, int64_t current_x)
 {
-	if (original_x > 0 && (LLONG_MAX / current_x) < current_x)
+	if (original_x > 0 && (LLONG_MAX / current_x) < original_x)
 	{
-		write(1, "START Result too big\n", 21);
+		write(1, "next POS POS Result too big\n", 28);
 		return (1);
 	}
 	if (original_x < 0)
 	{
-		// if (current_x > 0 && ((9223372036854775807 + 1) / current_x) < current_x)
+		if (current_x > 0)
 		{
-			write(1, "START Result too big\n", 21);
-			return (1);
+			printf("/ %lld\n",(-LLONG_MAX - 1) / -current_x);
+			if ((-LLONG_MAX - 1) / -current_x < -original_x)
+			{
+				write(1, "next NEG NEG Result too big\n", 28);
+				return (1);
+			}
+		}
+		if (current_x < 0)
+		{
+			printf("/ %lld\n",(-LLONG_MAX) / current_x);
+			if ((-LLONG_MAX) / current_x < -original_x)
+			{
+				write(1, "next NEG POS Result too big\n", 28);
+				return (1);
+			}
 		}
 	}
 
@@ -128,15 +94,9 @@ int64_t calc_result(int64_t x, int64_t y)
 	while (--y > 0)
 	{
 		x *= original_x;
-		printf("LOOP llmax / x : %lld\n", LLONG_MAX / x);
-		if (y > 1 && (LLONG_MAX / x) < original_x)
-		{
-			write(1, "POS Result too big\n", 19);
+		if (y != 1 && check_result(original_x, x))
 			return (0);
-		}
-		printf("y IN  : %lld\n", y);
 	}
-	printf("y OUT : %lld\n", y);
 	return (x);
 }
 
