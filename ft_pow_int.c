1da1ebf72327bb6be268bbc4c35aa5a76d7c6913
vphongph <vphongph@student.42.fr>
2019-03-13 01:01:01 +0100

    almost finished pow

--- ft_pow_int.c
+++ ft_pow_int.c
@@ -6,7 +6,7 @@
 /*   By: vphongph <vphongph@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/03/05 15:47:58 by vphongph          #+#    #+#             */
-/*   Updated: 2019/03/10 23:08:24 by vphongph         ###   ########.fr       */
+/*   Updated: 2019/03/11 22:59:56 by vphongph         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,9 +17,10 @@
 
 /*
 ** __Protection__
-** on divise val max par notre nombre,
-** si le quotient > nombre, trigger sécurité et renvoie 0
-**
+** on divise INT64 MAX par notre nombre,
+** si le quotient < nombre, trigger sécurité et renvoie 0
+** Attention pour les nombre < 0, le prochain nombre sera du signe opposé
+** et donc la division est différente selon le cas, INT64 MAX ou INT64 MIN
 */
 
 int8_t	check_result(int64_t original_x, int64_t current_x)
@@ -27,7 +28,10 @@ int8_t	check_result(int64_t original_x, int64_t current_x)
 	if (original_x > 0)
 	{
 		if (LLONG_MAX / current_x < original_x)
+		{
+			write(1, "Result too big\n", 15);
 			return (1);
+		}
 	}
 	if (original_x < 0)
 	{
@@ -35,32 +39,29 @@ int8_t	check_result(int64_t original_x, int64_t current_x)
 		{
 			if ((-LLONG_MAX - 1) / -current_x < -original_x)
 			{
-				write(1, "Result too big\n", 5);
+				write(1, "Result too big\n", 15);
 				return (1);
 			}
 		}
 		if (current_x < 0)
 		{
-			printf("3 / : %lld\n",(-LLONG_MAX) / current_x);
 			if ((-LLONG_MAX) / current_x < -original_x || (-LLONG_MAX) / current_x <= 1)
 			{
-				write(1, "Result too big\n", 5);
+				write(1, "Result too big\n", 15);
 				return (1);
 			}
 		}
 	}
-
 	return (0);
 }
 
 int64_t calc_result(int64_t x, int64_t y)
 {
-	int original_x;
+	int64_t original_x;
 
 	original_x = x;
 	while (--y > 0)
 	{
-		// printf("X : %lld\n", x * original_x);
 		x *= original_x;
 		if (y != 1 && check_result(original_x, x))
 			return (0);
