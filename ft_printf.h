cb0bdf8baac32f5e66b0183ae31e4c9dbcd29394
vphongph <vphongph@student.42.fr>
2019-04-03 05:58:23 +0200

    begin of big int

--- ft_printf.h
+++ ft_printf.h
@@ -6,14 +6,15 @@
 /*   By: vphongph <vphongph@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/03/04 17:11:36 by vphongph          #+#    #+#             */
-/*   Updated: 2019/03/19 20:06:53 by vphongph         ###   ########.fr       */
+/*   Updated: 2019/04/03 02:26:12 by vphongph         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef FT_PRINTF_H
 # define FT_PRINTF_H
+# include <i386/endian.h>
 # if __DARWIN_BYTE_ORDER == __DARWIN_LITTLE_ENDIAN
-#include "libft/libft.h"
+#  include "libft/libft.h"
 #  include <stdlib.h>
 
 typedef struct	s_format
@@ -23,6 +24,11 @@ typedef struct	s_format
 	int options;
 	int	type;
 }				t_format;
+
+typedef struct s_big_int
+{
+	const __uint128_t uint128_max;
+}				t_big_int;
 
 # endif
 #endif
